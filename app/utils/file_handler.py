8974400cd1cd4ab3a15f07e0f44d4d194ca16eec
SCHANDAN996 <work.chandansingh@gmail.com>
2025-07-12 13:52:57 +0530

    Update file_handler.py

--- app/utils/file_handler.py
+++ app/utils/file_handler.py
@@ -1,1 +1,11 @@
 
+from PyPDF2 import PdfMerger
+
+def merge_pdfs(files: list) -> str:
+    merger = PdfMerger()
+    for file in files:
+        merger.append(file)
+    output_pdf = "merged_output.pdf"
+    merger.write(output_pdf)
+    merger.close()
+    return output_pdf
