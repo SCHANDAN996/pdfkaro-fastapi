c76769e18090690c2ddc77f48f74e38a39e8d0c6
SCHANDAN996 <work.chandansingh@gmail.com>
2025-07-12 13:35:44 +0530

    Create split.py

---
+++ app/api/split.py
@@ -0,0 +1,9 @@
+from fastapi import HTTPException
+from app.utils.file_handler import split_pdf
+
+async def split_pdf(file: str, page_number: int):
+    try:
+        split_output = split_pdf(file, page_number)
+        return {"message": "PDF split successfully", "split_pdf": split_output}
+    except Exception as e:
+        raise HTTPException(status_code=500, detail=f"Error splitting PDF: {str(e)}")
