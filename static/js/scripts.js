06682992d47e13da9496bc4f8d3e10907cf79e9e
SCHANDAN996 <work.chandansingh@gmail.com>
2025-07-13 10:28:13 +0530

    Delete static directory

--- static/js/scripts.js
+++
@@ -1,9 +0,0 @@
-async function mergePDFs() {
-    const files = document.getElementById("pdfFiles").files;
-    const formData = new FormData();
-    for (let file of files) formData.append("files", file);
-
-    const response = await fetch("/merge", { method: "POST", body: formData });
-    const data = await response.json();
-    document.getElementById("result").innerHTML = `<a href="${data.output}">डाउनलोड करें</a>`;
-}
