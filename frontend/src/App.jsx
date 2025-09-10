98851f0ef835e2d686bef16bcc02b49dc9df8065
SCHANDAN996 <work.chandansingh@gmail.com>
2025-09-10 16:39:08 +0530

    Update App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,23 +1,20 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import Header from './components/Header.jsx';
-import Footer from './components/Footer.jsx';
-import HomePage from './pages/HomePage.jsx';
-import MergePage from './pages/MergePage.jsx';
-// We will create SplitPage later, so it's commented out for now.
-// import SplitPage from './pages/SplitPage'; 
+import Header from './components/Header';
+import Footer from './components/Footer';
+import HomePage from './pages/HomePage';
+import MergePage from './pages/MergePage';
 
 function App() {
   return (
     <Router>
-      <div className="flex flex-col min-h-screen bg-slate-50 font-sans">
+      <div className="flex flex-col min-h-screen bg-slate-50 text-slate-800 font-sans">
         <Header />
         <main className="flex-grow">
           <Routes>
             <Route path="/" element={<HomePage />} />
             <Route path="/merge" element={<MergePage />} />
-            {/* When you create SplitPage.jsx, you can uncomment the line below */}
-            {/* <Route path="/split" element={<SplitPage />} /> */}
+            {/* Add other routes like /split here later */}
           </Routes>
         </main>
         <Footer />
@@ -27,5 +24,4 @@ function App() {
 }
 
 export default App;
-
 
