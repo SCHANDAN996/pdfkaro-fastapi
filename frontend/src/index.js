aa1757cd116791be900e659d7dd871132c216270
SCHANDAN996 <work.chandansingh@gmail.com>
2025-09-20 16:12:10 +0530

    Update index.js

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,13 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
+import App from './pages/App.jsx'; 
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  // Humne yahan se <React.StrictMode> ko hata diya hai.
-  // Yeh drag-and-drop ki samasya ko 100% theek kar dega.
+  // StrictMode has been removed to fix react-beautiful-dnd issues
   <App />
 );
 
