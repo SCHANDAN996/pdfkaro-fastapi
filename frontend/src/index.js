07bfc7a4c4283c0698705bba8eccab3ff720ba34
SCHANDAN996 <work.chandansingh@gmail.com>
2025-09-21 12:00:46 +0530

    Update index.js

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -2,11 +2,11 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App.jsx';
-import reportWebVitals from './reportWebVitals';
+// import reportWebVitals from './reportWebVitals'; // <-- इस लाइन को हटा दिया गया है
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <App />
 );
 
-reportWebVitals();
+// reportWebVitals(); // <-- इस लाइन को भी हटा दिया गया है
