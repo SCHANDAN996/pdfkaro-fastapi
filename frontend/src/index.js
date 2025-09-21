33e34a9428a8bf34f9b050b5d9bc0cc09feed57f
SCHANDAN996 <work.chandansingh@gmail.com>
2025-09-21 11:09:07 +0530

    Update index.js

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,12 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './pages/App.jsx'; 
+import App from './App.jsx';
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  // StrictMode has been removed to fix react-beautiful-dnd issues
   <App />
 );
 
